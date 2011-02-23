6a896d6669df0e7b86afac9f7c8c066f0a2a9755
Manung Han <manung@gmail.com>
2011-02-23 17:20:45 -0500

    Add Cedar spec template

---
+++ Xcode/File Templates/Cocoa/Cedar spec.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,22 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <Cedar/SpecHelper.h>
+#import <OCMock/OCMock.h>
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
+SPEC_BEGIN(___FILEBASENAMEASIDENTIFIER___)
+describe(@"___FILEBASENAMEASIDENTIFIER___", ^{
+    beforeEach(^{
+    });
+    
+    it(@"should do something", ^{
+    });
+});
+SPEC_END
---
+++ Xcode/README.markdown
@@ -0,0 +1,9 @@
+# Xcode
+
+## File Templates
+
+### Setup
+Copy "File Templates" folder to  ~/Library/Developer/Xcode/Templates
+
+### Description
+Cedar Spec - File template for developing a [Cedar](https://github.com/pivotal/cedar) spec.
